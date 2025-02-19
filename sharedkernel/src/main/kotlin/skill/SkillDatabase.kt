59a3660140afe9fb7860809163e30a8ac0c5c04d
BenoitDeligny <delignyb.pro@gmail.com>
2025-02-19 13:52:31 +0100

    Fill skills data

--- sharedkernel/src/main/kotlin/skill/SkillDatabase.kt
+++ sharedkernel/src/main/kotlin/skill/SkillDatabase.kt
@@ -5,8 +5,21 @@ class SkillDatabase {
     private val skills = mutableSetOf<SkillEntity>()
 
     init {
-        skills.add(SkillEntity(id = 1, name = "Shooting"))
-        skills.add(SkillEntity(id = 2, name = "Swimming"))
+        skills.add(SkillEntity(id = 1, name = "Acrobatie"))
+        skills.add(SkillEntity(id = 2, name = "Crochetage"))
+        skills.add(SkillEntity(id = 3, name = "Déminage"))
+        skills.add(SkillEntity(id = 4, name = "Discretion"))
+        skills.add(SkillEntity(id = 5, name = "Escalade"))
+        skills.add(SkillEntity(id = 6, name = "Escrime"))
+        skills.add(SkillEntity(id = 7, name = "Lutte"))
+        skills.add(SkillEntity(id = 8, name = "Natation"))
+        skills.add(SkillEntity(id = 9, name = "Observation"))
+        skills.add(SkillEntity(id = 10, name = "Pilotage"))
+        skills.add(SkillEntity(id = 11, name = "Premier soins"))
+        skills.add(SkillEntity(id = 12, name = "Réparation"))
+        skills.add(SkillEntity(id = 13, name = "Sport"))
+        skills.add(SkillEntity(id = 14, name = "Survie"))
+        skills.add(SkillEntity(id = 15, name = "Tir"))
     }
 
     fun getSkill(id: Int): SkillEntity? = skills.find { it.id == id }
